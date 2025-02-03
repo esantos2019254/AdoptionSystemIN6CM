7639e248da14b38c06c67d4623dcfe99d6cedd31
esantos2019254 <esantos-2019254@kinal.edu.gt>
2025-02-03 08:38:51 -0600

    Proyecto Login y register funcionando

---
+++ src/users/user.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: [true, "El nombre es requerido"],
+    },
+    correo: {
+        type: String,
+        required: [true, "El correo es requerido"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "La contraseña es requerida"],
+    },
+    img: {
+        type: String,
+    },
+    phone: {
+        type: String,
+        minLength: 8,
+        maxLength: 8,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["ADMIN_ROLE", "USER_ROLE"],
+    },
+    estado: {
+        type: Boolean,
+        default: true,
+    },
+    google: {
+        type: Boolean,
+        default: false,
+    }
+});
+
+UserSchema.methods.toJSON = function () {
+    const { __v, password, _id, ...usuario} = this.toObject();
+    usuario.uid = _id;
+    return usuario;
+}
+
+export default mongoose.model('User', UserSchema);
